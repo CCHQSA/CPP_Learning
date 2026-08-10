88ca071acd58dd08252b81074a60515e1cfdab4b
mykola <boberanonim5@gmail.com>
2026-08-10 20:17:36 +0300

    Added history

--- bank_system.cpp
+++ bank_system.cpp
@@ -2,15 +2,17 @@
 #include <string>
 #include <ctime>
 #include <iomanip>
-
+#include <vector>
 
 void showBalance(double balance);
-double deposit();
-double withdraw(double balance);
+double deposit(std::vector<std::string>& history);
+double withdraw(double balance, std::vector<std::string>& history);
+void showHistory(const std::vector<std::string>& history);
 
 int main() {
     double balance = 0;
     int choice = 0;
+    std::vector<std::string> history = {};
 
     do {
 
@@ -18,31 +20,35 @@ int main() {
         std::cout << "1. Show Balance" << std::endl;
         std::cout << "2. Deposit" << std::endl;
         std::cout << "3. Withdraw" << std::endl;
-        std::cout << "4. Exit" << std::endl;
+        std::cout << "4. History" << std::endl;
+        std::cout << "5. Exit" << std::endl;
         std::cout << "Enter your choice: ";
         std::cin >> choice;
 
         std::cin.clear(); 
-        fflush(stdin);
+        std::cin.ignore(10000, '\n');
         switch(choice) {
             case 1:
                 showBalance(balance);
                 break;
             case 2:
-                balance += deposit();
+                balance += deposit(history);
                 showBalance(balance); 
                 break;
             case 3:
-                balance -= withdraw(balance);
+                balance -= withdraw(balance, history);
                 showBalance(balance); 
                 break;
             case 4:
+                showHistory(history);
+                break;
+            case 5:
                 std::cout << "Exiting the program. Thank you!" << std::endl;
                 break;
             default:
                 std::cout << "Invalid choice. Please try again." << std::endl;
         }
-    } while(choice != 4);
+    } while(choice != 5);
 
     return 0;
 }
@@ -51,7 +57,7 @@ void showBalance(double balance) {
     std::cout << "Your current balance is: $" << std::fixed << std::setprecision(2) << balance << std::endl;
 }
 
-double deposit() {
+double deposit(std::vector<std::string>& history) {
     double amount = 0;
     std::cout << "Enter the amount to deposit: ";
     std::cin >> amount;
@@ -60,10 +66,12 @@ double deposit() {
         std::cout << "Invalid amount. Deposit failed." << std::endl;
         return 0;
     }
+
+    history.push_back("Deposit: +$" + std::to_string(amount));
     return amount;
 }
 
-double withdraw(double balance) {
+double withdraw(double balance, std::vector<std::string>& history) {
     double amount = 0;
     std::cout << "Enter the amount to withdraw: ";
     std::cin >> amount;
@@ -77,5 +85,19 @@ double withdraw(double balance) {
         std::cout << "Insufficient funds. Withdrawal failed." << std::endl;
         return 0;
     }
+
+    history.push_back("Withdraw: -$" + std::to_string(amount));
+
     return amount;
+}
+
+void showHistory(const std::vector<std::string>& history) {
+    std::cout << "\n--- TRANSACTION HISTORY ---\n";
+    if (history.empty()) {
+        std::cout << "No transactions yet.\n";
+        return;
+    }
+    for (int i = 0; i < history.size(); i++) {
+        std::cout << history[i] << std::endl;
+    }
 }
