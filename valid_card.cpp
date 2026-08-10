04ef1d83a07846ce9ba7b480a6b7ec98d0eed678
mykola <boberanonim5@gmail.com>
2026-08-10 11:59:53 +0300

    Better solution for valid card

--- valid_card.cpp
+++ valid_card.cpp
@@ -1,67 +1,43 @@
 #include <iostream>
+#include <string>
 
-
-std::string doubleNums(const std::string& number);
-int addNums(const std::string &numbers);
-int addOddNums(const std::string &numbers);
-bool valid(int &num1, int &num2);
+int getDigits(const int number);
+int sumEven(const std::string cardNumber);
+int sumOdd(const std::string cardNumber);
 
 int main(){
-  
-  std::string number;
+    std::string cardNumber;
 
-  std::cout << "Enter a credit card number: ";
-  std::cin >> number;
+    std::cout << "Enter a credit card number: ";
+    std::cin >> cardNumber;
 
-  std::string doubled = doubleNums(number);
-  int firstValid = addNums(doubled);
-  int secondValid = addOddNums(number);
-  bool isValid = valid(firstValid, secondValid);
-  if (isValid) {
-    std::cout << "Card is valid";
-  }else{
-    std::cout << "Card is invalid";
-  }
-  
-}
-
-
-std::string doubleNums(const std::string &number){
-  std::string doubled;
-  for(int i = number.length() - 1; i >= 0; i--){
-
-    if((number.length() - 1 - i) % 2 == 1){
-      std::string num = std::to_string((number[i] - '0') * 2);
-      doubled.insert(0, num);
+    int result = sumEven(cardNumber) + sumOdd(cardNumber);
+    
+    if (result % 10 == 0){
+        std::cout << "Card is valid\n";
+    } else {
+        std::cout << "Card is invalid\n";
     }
-  }
-  return doubled;
+  
+    return 0;
 }
 
-int addNums(const std::string &numbers){
-  int res = 0;
-  for(int i = 0; i < numbers.length(); i++){
-    res += (numbers[i] - '0');
-  }
-  return res;
+int getDigits(const int number){
+    return number % 10 + (number / 10 % 10);
 }
 
-int addOddNums(const std::string &numbers){
-  int res = 0;
-  for(int i = numbers.length() - 1; i >= 0; i--){
-    if((numbers.length() - 1 - i) % 2 == 0){
-      int num = (numbers[i] - '0');
-      res += num;
+int sumEven(const std::string cardNumber){
+    int res = 0;
+    for(int i = cardNumber.size() - 2; i >= 0; i -= 2){
+        res += getDigits((cardNumber[i] - '0') * 2);
     }
-  }
-  return res;
+    return res;
 }
 
-bool valid(int &num1, int &num2){
-  if ((num1 + num2) % 10 == 0){
-    return true; 
-  }else{
-    return false;
-  } 
-
+int sumOdd(const std::string cardNumber){
+    int res = 0;
+    for(int i = cardNumber.size() - 1; i >= 0; i -= 2){
+        res += cardNumber[i] - '0';
+    }
+    return res;
 }
